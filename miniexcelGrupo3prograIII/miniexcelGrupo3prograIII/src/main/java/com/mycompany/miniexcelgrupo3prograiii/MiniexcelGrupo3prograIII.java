4e9d03b3e07188ae332577ee0d875cc4c6b2c825
tahual <161674586+tahual@users.noreply.github.com>
2025-03-04 20:37:18 -0600

    carpeta en la que vamos a trabajar miniexcel
    creacon de carpeta en la rama main para crear nuestro mini excel

---
+++ miniexcelGrupo3prograIII/miniexcelGrupo3prograIII/src/main/java/com/mycompany/miniexcelgrupo3prograiii/MiniexcelGrupo3prograIII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniexcelgrupo3prograiii;
+
+/**
+ *
+ * @author danyt
+ */
+public class MiniexcelGrupo3prograIII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
