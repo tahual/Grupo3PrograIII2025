89290f13a1a63dc7ee74f9e6143c019947e3ccf9
danyt <danyt@MSI>
2025-05-08 22:37:48 -0600

    integracio de menu

--- miniexcelGrupo3prograIII/miniexcelGrupo3prograIII/src/main/java/com/mycompany/miniexcelgrupo3prograiii/MiniexcelGrupo3prograIII.java
+++ miniexcelGrupo3prograIII/miniexcelGrupo3prograIII/src/main/java/com/mycompany/miniexcelgrupo3prograiii/MiniexcelGrupo3prograIII.java
@@ -4,16 +4,39 @@
 
 package com.mycompany.miniexcelgrupo3prograiii;
 
+import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 /**
  *
  * @author danyt
  */
 public class MiniexcelGrupo3prograIII {
-      public static void main(String[] args) {
-        // Crear la ventana principal
+     public static void main(String[] args){
+     Scanner scanner = new Scanner(System.in); 
+     int opcion= 0; 
+
+    do{
+        
+        System.out.println("Ingrese el trabajo que quiere realizar: ");
+        System.out.println("1. Arbol Binario) ");
+        System.out.println("2. AVL) ");
+        System.out.println("3. Mini Excel");
+        System.out.println("4. Salir");
+        
+        try{
+        
+        opcion = scanner.nextInt(); 
+        scanner.nextLine(); 
+        
+    
+    switch(opcion){
+        case 1:
+            break;
+        case 2: 
+            break;
+        case 3:
+               // Crear la ventana principal
         JFrame frame = new JFrame("Mini Excel");
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,6 +48,23 @@ public class MiniexcelGrupo3prograIII {
         // Agregar el panel a la ventana y hacerla visible
         frame.add(panel);
         frame.setVisible(true);
+            break;
+        case 4:
+            break;
+            
+            default:
+                System.out.println("la opcion que ingreso no es valida");
+            
+        }
+        
+        }catch(Exception e){
+            System.out.println("Errror ingrese un numero valido. ");
+            scanner.nextLine(); 
+        }
+    
+    }while(opcion != 4); 
+    
+    
     }
 }
 
